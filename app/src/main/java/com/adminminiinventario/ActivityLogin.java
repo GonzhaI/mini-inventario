ebeeeabc9ffe3326573e1147d9acab2926c3b64e
Alexis0247 <133908930+Alexis0247@users.noreply.github.com>
2023-09-23 18:47:47 -0300

    Base de pantalla principal
    Hice la base de la pantalla principal (Inicio, Login, Registro), falta el back, que serian las validaciones y todo eso, toca esperar para consultarle algunas cosas al profe para arreglar y tamo.

---
+++ app/src/main/java/com/adminminiinventario/ActivityLogin.java
@@ -0,0 +1,19 @@
+package com.adminminiinventario;
+
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ActivityLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
