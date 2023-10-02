f6224e2523e99262299e03eba527bfaf745cc4eb
DanielU94 <134022544+DanielU94@users.noreply.github.com>
2023-10-02 01:33:33 -0300

    Registro1

--- app/src/main/java/com/adminminiinventario/MainActivity.java
+++ app/src/main/java/com/adminminiinventario/MainActivity.java
@@ -1,5 +1,6 @@
 package com.adminminiinventario;
 
+import com.google.firebase.firestore.FirebaseFirestore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,17 +8,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseFirestore db;
+
     Button bt_ingresar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bt_ingresar=(Button)findViewById(R.id.bt_registrarse);
 
-        bt_ingresar.setOnClickListener(new View.OnClickListener(){
+        // Inicializa Firestore
+        db = FirebaseFirestore.getInstance();
+
+        bt_ingresar = findViewById(R.id.bt_registrarse);
+
+        bt_ingresar.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
@@ -26,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     public void irLogin(View view) {
         // Crear un Intent para ir a la otra actividad
         Intent intent = new Intent(this, ActivityLogin.class);
