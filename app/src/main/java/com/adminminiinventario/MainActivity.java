e904b003f17ad19e7df0c53e7fa62452b082bfe5
CristofferG <134312678+CristofferG@users.noreply.github.com>
2023-10-02 11:08:39 -0300

    No va a quedar la caga

--- app/src/main/java/com/adminminiinventario/MainActivity.java
+++ app/src/main/java/com/adminminiinventario/MainActivity.java
@@ -11,20 +11,32 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button bt_ingresar;
+    Button bt_prueba;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bt_ingresar=(Button)findViewById(R.id.bt_registrarse);
+        bt_prueba=(Button)findViewById(R.id.bt_prueba);
 
         bt_ingresar.setOnClickListener(new View.OnClickListener(){
-
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, ActivityRegistrarse.class);
                 startActivity(i);
             }
         });
+
+        bt_prueba.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, ActivityCalendario.class);
+                startActivity(i);
+            }
+        });
+
+
+
     }
     public void irLogin(View view) {
         // Crear un Intent para ir a la otra actividad
