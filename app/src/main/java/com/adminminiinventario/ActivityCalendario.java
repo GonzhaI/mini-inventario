f3d27282b6a2cbe336471c99ffa8c4c611f2610b
CristofferG <134312678+CristofferG@users.noreply.github.com>
2023-10-03 21:40:47 -0300

    Conexión a base de datos Calendario

--- app/src/main/java/com/adminminiinventario/ActivityCalendario.java
+++ app/src/main/java/com/adminminiinventario/ActivityCalendario.java
@@ -10,12 +10,25 @@ import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+import androidx.annotation.NonNull;
 
 public class ActivityCalendario extends AppCompatActivity {
     TextView tv;
     Button botonFechaP;
     private String fecha;
+
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +36,7 @@ public class ActivityCalendario extends AppCompatActivity {
         setContentView(R.layout.activity_calendario);
         tv = findViewById(R.id.fecha);
         botonFechaP = findViewById(R.id.botonFecha);
+        db = FirebaseFirestore.getInstance();
     }
 
     public void abrirCalendario(View view) {
@@ -34,17 +48,37 @@ public class ActivityCalendario extends AppCompatActivity {
         DatePickerDialog dpd = new DatePickerDialog(ActivityCalendario.this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
-             fecha = dayOfMonth + "/" + (month + 1) + "/" + year;
-             tv.setText(fecha);
+                fecha = dayOfMonth + "/" + (month + 1) + "/" + year;
+                tv.setText(fecha);
             }
         }, anio, mes, dia);
         dpd.show();
     }
-    public void MostrarMensaje(View view){
-        if (fecha != null && !fecha.isEmpty()){
-            Toast.makeText(this, "Se ha guardado correctamente!", Toast.LENGTH_SHORT).show();
+
+    public void MostrarMensaje(View view) {
+        if (fecha != null && !fecha.isEmpty()) {
+            // Aquí, enviamos la fecha a Firebase Firestore
+            Map<String, Object> datos = new HashMap<>();
+            datos.put("Fecha", fecha);
+
+            // "fechaPrueba" es el nombre de tu colección en Firestore
+            db.collection("fechaPrueba").add(datos)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            // Manejar el éxito
+                            Toast.makeText(ActivityCalendario.this, "Fecha guardada correctamente!", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            // Manejar el fallo
+                            Toast.makeText(ActivityCalendario.this, "Error al guardar la fecha", Toast.LENGTH_SHORT).show();
+                        }
+                    });
         } else {
-            Toast.makeText(this, "No se ha ingresado una fecha", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "No se ha seleccionado una fecha", Toast.LENGTH_SHORT).show();
         }
     }
 }
