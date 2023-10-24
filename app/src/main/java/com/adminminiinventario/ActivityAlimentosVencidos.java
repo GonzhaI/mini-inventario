7229e4f6da4e8813193ac0e4f7a9e855cc97d5a4
CristofferG <134312678+CristofferG@users.noreply.github.com>
2023-10-23 23:44:16 -0300

    Ver productos vencidos.V2
    Antes mostraba todos los productos en la base de datos. Ahora muestra los productos del usuarios que ha ingresado  en la tabla y ordena las fechas de menor a mayor.

--- app/src/main/java/com/adminminiinventario/ActivityAlimentosVencidos.java
+++ app/src/main/java/com/adminminiinventario/ActivityAlimentosVencidos.java
@@ -11,20 +11,31 @@ import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class ActivityAlimentosVencidos extends AppCompatActivity {
     private FirebaseFirestore db;
     private TableLayout tableLayoutProductos;
+    private TextView nombreUsuario;
+    private boolean headersAdded = false;
+    private List<Productos> productosMostrados = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +44,47 @@ public class ActivityAlimentosVencidos extends AppCompatActivity {
 
         db = FirebaseFirestore.getInstance();
         tableLayoutProductos = findViewById(R.id.tableLayoutAlimentosPorVencer);
+        nombreUsuario = findViewById(R.id.nombre_usuario);
 
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (currentUser != null) {
+            String uid = currentUser.getUid();
+
+            db.collection("usuario")
+                    .document(uid)
+                    .get()
+                    .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                        @Override
+                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                            if (documentSnapshot.exists()) {
+                                String negocio = documentSnapshot.getString("negocio").toLowerCase();
+                                nombreUsuario.setText(negocio);
+
+                                db.collection("productos")
+                                        .whereEqualTo("id_negocio", negocio)
+                                        .get()
+                                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                                if (task.isSuccessful()) {
+                                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                                        Productos producto = document.toObject(Productos.class);
+                                                        agregarProductoALaTabla(producto);
+                                                    }
+                                                    mostrarProductosEnTablaOrdenada();
+                                                } else {
+                                                    Log.e("TuActivity", "Error al obtener los productos", task.getException());
+                                                }
+                                            }
+                                        });
+                            }
+                        }
+                    });
+        }
+    }
+
+    private void agregarProductoALaTabla(Productos producto) {
         // Obtener la fecha actual
         Calendar calendar = Calendar.getInstance();
         Date fechaActual = calendar.getTime();
@@ -42,88 +93,83 @@ public class ActivityAlimentosVencidos extends AppCompatActivity {
         calendar.add(Calendar.DAY_OF_YEAR, 14);
         Date fechaVencimientoMaxima = calendar.getTime();
 
-        db.collection("productos")
-                .whereGreaterThan("fechaVencimiento", fechaActual)
-                .whereLessThan("fechaVencimiento", fechaVencimientoMaxima)
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Productos producto = document.toObject(Productos.class);
-                                agregarProductoALaTabla(producto);
-                            }
-                        } else {
-                            Log.e("TuActivity", "Error al obtener los productos", task.getException());
-                        }
-                    }
-                });
+        // Obtener la fecha de vencimiento del producto
+        Date fechaVencimiento = producto.getfechaVencimiento().toDate();
+
+        // Verificar si el producto está a punto de vencer
+        if (fechaVencimiento.after(fechaActual) && fechaVencimiento.before(fechaVencimientoMaxima)) {
+            productosMostrados.add(producto);
+        }
     }
 
-    private boolean headersAdded = false; // Variable para verificar si los encabezados ya han sido agregados
-
-    private void agregarProductoALaTabla(Productos producto) {
-        TableRow newRow = new TableRow(this);
-
-        TextView textViewNombreProducto = new TextView(this);
-        textViewNombreProducto.setText(producto.getProducto());
-        textViewNombreProducto.setGravity(Gravity.CENTER); // Centrar el texto
-
-        TextView textViewCantidad = new TextView(this);
-        textViewCantidad.setText(String.valueOf(producto.getCantidad()));
-        textViewCantidad.setGravity(Gravity.CENTER); // Centrar el texto
-
-        TextView textViewFechaVencimiento = new TextView(this);
-        Timestamp fechaVencimiento = producto.getfechaVencimiento();
-
-        if (fechaVencimiento != null) {
-            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
-            String fechaFormateada = sdf.format(fechaVencimiento.toDate());
-            textViewFechaVencimiento.setText(fechaFormateada);
-        } else {
-            textViewFechaVencimiento.setText("N/A");
-        }
-        textViewFechaVencimiento.setGravity(Gravity.CENTER); // Centrar el texto
-
-        // Configurar espacio de columnas
-        textViewNombreProducto.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1f));
-        textViewCantidad.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1f));
-        textViewFechaVencimiento.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1f));
-
-        if (!headersAdded) {
-            // Crear TextViews para los títulos
-            TextView headernombreProducto = new TextView(this);
-            headernombreProducto.setText("Nombre del Producto");
-
-            TextView headerCantidad = new TextView(this);
-            headerCantidad.setText("Cantidad");
-
-            TextView headerfechaVencimiento = new TextView(this);
-            headerfechaVencimiento.setText("Fecha de Vencimiento");
-
-            // Agregar titulos a las columnas
-            TableRow headerRow = new TableRow(this);
-            headerRow.addView(headernombreProducto);
-            headerRow.addView(headerCantidad);
-            headerRow.addView(headerfechaVencimiento);
-
-            // Agregar la fila de títulos a la parte superior de la tabla
-            tableLayoutProductos.addView(headerRow, 0);
-
-            headersAdded = true; // Marcar que los encabezados han sido agregados
-        }
-
-        // Agrega las otras columnas según necesites
-        newRow.addView(textViewNombreProducto);
-        newRow.addView(textViewCantidad);
-        newRow.addView(textViewFechaVencimiento);
-
-        // Agrega la fila a la tabla
-        tableLayoutProductos.addView(newRow);
+    private void ordenarProductosPorFecha() {
+        Collections.sort(productosMostrados, new Comparator<Productos>() {
+            @Override
+            public int compare(Productos producto1, Productos producto2) {
+                Date fecha1 = producto1.getfechaVencimiento().toDate();
+                Date fecha2 = producto2.getfechaVencimiento().toDate();
+                return fecha1.compareTo(fecha2);
+            }
+        });
     }
 
+    private void mostrarProductosEnTablaOrdenada() {
+        ordenarProductosPorFecha();
 
+        for (Productos producto : productosMostrados) {
+            TableRow newRow = new TableRow(this);
 
+            TextView textViewNombreProducto = new TextView(this);
+            textViewNombreProducto.setText(producto.getProducto());
+            textViewNombreProducto.setGravity(Gravity.CENTER);
+
+            TextView textViewCantidad = new TextView(this);
+            textViewCantidad.setText(String.valueOf(producto.getCantidad()));
+            textViewCantidad.setGravity(Gravity.CENTER);
+
+            TextView textViewFechaVencimiento = new TextView(this);
+
+            Date fechaVencimiento = producto.getfechaVencimiento().toDate();
+
+            if (fechaVencimiento != null) {
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+                String fechaFormateada = sdf.format(fechaVencimiento);
+                textViewFechaVencimiento.setText(fechaFormateada);
+            } else {
+                textViewFechaVencimiento.setText("N/A");
+            }
+
+            textViewFechaVencimiento.setGravity(Gravity.CENTER);
+
+            textViewNombreProducto.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1f));
+            textViewCantidad.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1f));
+            textViewFechaVencimiento.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1f));
+
+            if (!headersAdded) {
+                TextView headernombreProducto = new TextView(this);
+                headernombreProducto.setText("Nombre del Producto");
+
+                TextView headerCantidad = new TextView(this);
+                headerCantidad.setText("Cantidad");
+
+                TextView headerfechaVencimiento = new TextView(this);
+                headerfechaVencimiento.setText("Fecha de Vencimiento");
+
+                TableRow headerRow = new TableRow(this);
+                headerRow.addView(headernombreProducto);
+                headerRow.addView(headerCantidad);
+                headerRow.addView(headerfechaVencimiento);
+
+                tableLayoutProductos.addView(headerRow, 0);
+
+                headersAdded = true;
+            }
+
+            newRow.addView(textViewNombreProducto);
+            newRow.addView(textViewCantidad);
+            newRow.addView(textViewFechaVencimiento);
+
+            tableLayoutProductos.addView(newRow);
+        }
+    }
 }
-
